521760eda82c8fd54557c06b3022b48eacbf1b62
Viktor H. Morales <viktorhugomorales@gmail.com>
2022-09-07 15:35:49 -0300

    add new routes and update comments

--- src/routes/v1/home.ts
+++ src/routes/v1/home.ts
@@ -1,12 +1,29 @@
+// Import ExpresJS framework
 import * as express from "express";
 
 // Set Router
 const homeRouter = express.Router();
 
+/**
+ * GET / (base)
+ * 
+ * @return {object}
+ */
 homeRouter.get("/", async (req, res) => {
     res.json({
       msg:"Show welcome message."
     });
+});
+
+/**
+ * GET /about
+ * 
+ * @return {object}
+ */
+homeRouter.get("/about", async (req, res) => {
+  res.json({
+    msg:"Show about information."
+  });
 });
 
 export default homeRouter;
--- src/routes/v1/posts.ts
+++ src/routes/v1/posts.ts
@@ -3,10 +3,40 @@ import * as express from "express";
 // Set Router
 const postRouter = express.Router();
 
+/**
+ * GET / (base)
+ * 
+ * @return {object} - All posts as Object
+ */
 postRouter.get("/", async (req, res) => {
     res.json({
       msg: "Get all posts"
     });
+});
+
+/**
+ * GET /:id
+ * 
+ * @param {number} - Post ID
+ * @return {object} - Post as Object
+ */
+postRouter.get("/:id", async (req, res) => {
+  res.json({
+    msg: `Get post with ID = ${req.params.id}`
+  });
+});
+
+/**
+ * POST /
+ * 
+ * @param {object} - Post information
+ * @return {object} - HTTP status
+ */
+postRouter.post("/", async (req, res) => {
+  res.json({
+    data: req.body,
+    msg: "New post added fakely"
+  });
 });
 
 export default postRouter;
