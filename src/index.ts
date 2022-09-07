988f4ad0a405d5868971946f95505bba3e4974a6
Viktor H. Morales <viktorhugomorales@gmail.com>
2022-09-07 15:35:37 -0300

    add new modules and update comments

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,29 @@
+// Import ExpressJS framework
 import * as express from "express";
-// Initialize and configure dotenv
+// Import and configure dotenv
 import * as dotenv from "dotenv";
 dotenv.config();
 
-// Import routes
+// Import modules related to routes
 import homeRouter from "./routes/v1/home";
 import postsRouter from "./routes/v1/posts";
 
-// Initialize Express server
+// Initialize ExpressJS server
 const app = express();
 const port = process.env.SERVER_PORT;
 
-// Routes V1
+// For parsing application/json POST DATA
+app.use(express.json());
+// For parsint application/x-www-form-urlencoded POST DATA
+app.use(express.urlencoded({ extended: true }));
+
+// Create routes
+// your-domain.com/v1 - Imports the homeRouter module
 app.use("/v1", homeRouter);
+// your-domain.com/v1/posts - Imports the postsRouter module
 app.use("/v1/posts", postsRouter);
 
-// start the Express server
+// Start the ExpressJS server
 app.listen( port, () => {
     console.log( `Server started at ${ port }` );
 } );
